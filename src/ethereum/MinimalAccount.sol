e5a96672301f91c43e316d53878d2d89d60ca5d6
Vivek <vivek.mitra@outlook.com>
2024-08-17 19:23:55 +1000

    validateUserOp and signature veriification logic wip

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -1,2 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract MinimalAccount is IAccount, Ownable {
+    constructor() Ownable(msg.sender) {}
+
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        returns (uint256 validationData)
+    {
+        _validateSignature(userOp, userOpHash);
+    }
+
+    //userOpHash fed to this function is the EIP 191 version of the signed hash, this needs to be converted to a normal hash
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
+        internal
+        view
+        returns (uint256 validationData)
+    {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+    }
+}
